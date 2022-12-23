f09956c7b909a8944010f2c8f6b8a4bf415169a2
thienhuynh98 <thien.huynh@bellevuecollege.edu>
2022-12-23 14:07:02 -0800

    affda

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, Switch, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Style Gen Studio</Text>
+        <Image
+            source={require('./assets/company-image.png')}
+            style={{ width: 160, height: 128 }}
+        />
+        <View style={styles.checkboxContainer}>
+          <Switch
+              value={isChecked}
+              onValueChange={() => setIsChecked(!isChecked)}
+          />
+          <Text style={styles.checkboxLabel}>I agree to the terms</Text>
+        </View>
+        <Button
+            title="Continue"
+            disabled={!isChecked}
+            onPress={() => console.log('Button pressed!')}
+        />
+      </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#333333"
   },
-});
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    color: 'white'
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  checkboxLabel: {
+    marginLeft: 10,
+  },
+  getStartedButton: {
+    backgroundColor: '#D5AB85',
+    borderRadius: 10
+  }
+};
+
+export default App;
